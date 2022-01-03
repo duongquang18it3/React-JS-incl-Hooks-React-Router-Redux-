4ffbfc141404b7527560b7527d384a694bb9433f
duongquang18it3 <tdquang.18it3@sict.udn.vn>
2022-01-03 15:46:00 +0700

    Arrow Functions

--- JavaScripts Refresher/script.js
+++ JavaScripts Refresher/script.js
@@ -1,5 +1,33 @@
+//1.var,let and const
 var namer = "Quang";
 console.log(namer);
-
 namer = "Thu";
 console.log(namer);
+//let
+let myName = "Max";
+console.log(myName);
+myName = "Quang";
+console.log(myName);
+//const
+const ten = "Bong";
+console.log(ten);
+const ten1 = "Tuyet Thu";
+console.log(ten1);
+
+// Arrow Functions and Normal Functions
+// Normal functions
+
+function printMyName(name) {
+  console.log(name);
+}
+printMyName("Quang handsome");
+
+// Arrow Functions
+
+const inTen = (ten) => {
+  console.log(ten);
+};
+inTen("Quang dep trai");
+
+const multiply = (number) => number * 2;
+console.log(multiply(4));
