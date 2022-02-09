28cfae96213fb537601b9210e098dcafef252a1e
Tran Duong Quang <tdquang.18IT3@sict.udn.vn>
2022-02-09 22:54:06 +0700

    adding-normal-javascript-logic

--- 03 React basic and working with components/my-react-app/src/App.js
+++ 03 React basic and working with components/my-react-app/src/App.js
@@ -44,6 +44,11 @@ function App() {
         amount={expenses[2].amount}
         date={expenses[2].date}
       ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
