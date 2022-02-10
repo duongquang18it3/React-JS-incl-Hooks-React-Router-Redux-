caffd0a982c5122911ccc1dc6c2361c13415518f
Tran Duong Quang <tdquang.18IT3@sict.udn.vn>
2022-02-10 23:07:32 +0700

    the-concept-of-composition

--- 03 React basic and working with components/my-react-app/src/components/Expenses.js
+++ 03 React basic and working with components/my-react-app/src/components/Expenses.js
@@ -1,9 +1,9 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 function Expenses(props) {
   return (
-    <div className="expenses">
-      
+    <Card className="expenses">
       <ExpenseItem
         title={props.item[0].title}
         amount={props.item[0].amount}
@@ -24,7 +24,7 @@ function Expenses(props) {
         amount={props.item[3].amount}
         date={props.item[3].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 export default Expenses;
