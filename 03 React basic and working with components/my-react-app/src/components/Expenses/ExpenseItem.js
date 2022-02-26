b6d4fa48d47fc6fa0141d8b956e15b439444684d
Tran Duong Quang <tdquang.18it3@sict.udn.vn>
2022-02-26 16:24:31 +0700

    working with state

--- 03 React basic and working with components/my-react-app/src/components/Expenses/ExpenseItem.js
+++ 03 React basic and working with components/my-react-app/src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    title = "Updated !";
+    setTitle("Updated !");
     console.log(title);
   };
   return (
