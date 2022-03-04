ff7d35e84dc1ba60c4f901b19d4c42ac75bf8fe5
Tran Duong Quang <tdquang.18it3@sict.udn.vn>
2022-03-04 23:20:38 +0700

    04-updating-state-that-depends-on-previous-state

--- 03 React basic and working with components/my-react-app/src/components/NewExpense/ExpenseForm.js
+++ 03 React basic and working with components/my-react-app/src/components/NewExpense/ExpenseForm.js
@@ -5,19 +5,58 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  //In this case, I wwant show you approach syntax alternatives
+  // You can use overwrite propertise on object with spread operator
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // But on above case, It will fail in some case, If you use to increase number or count
+    // You must use syntax below
+
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+
+    // prevState is useState({enteredTitle: "",enteredAmount: "",enteredDate: "",});
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: enteredDate,
+    };
+    console.log(expenseData);
   };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
