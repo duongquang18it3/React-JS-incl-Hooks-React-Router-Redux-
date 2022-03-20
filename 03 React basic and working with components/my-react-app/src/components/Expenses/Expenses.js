076ae0e6916bfec6927e434dadee19e6a94c267c
Tran Duong Quang <tdquang.18it3@sict.udn.vn>
2022-03-20 21:23:28 +0700

    05-rendering-lists-conditional-content 01-using-stateful-lists

--- 03 React basic and working with components/my-react-app/src/components/Expenses/Expenses.js
+++ 03 React basic and working with components/my-react-app/src/components/Expenses/Expenses.js
@@ -7,35 +7,24 @@ const Expenses = (props) => {
   const [filteredYear, setFilterYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
-    };
+  };
 
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        date={props.item[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        date={props.item[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        date={props.item[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        date={props.item[3].date}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 export default Expenses;
--- 03 React basic and working with components/my-react-app/src/components/NewExpense/ExpenseForm.js
+++ 03 React basic and working with components/my-react-app/src/components/NewExpense/ExpenseForm.js
@@ -51,7 +51,7 @@ const ExpenseForm = (props) => {
     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: enteredDate,
+      date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
